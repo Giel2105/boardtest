219055674e33cc3d84afc4a08d931dfa0a9c663c
Giel <g.stapelkamp@hotmail.nl>
2024-01-22 11:51:04 +0100

    changes

--- CodeExamples/boardtest/boardtest.ino
+++ CodeExamples/boardtest/boardtest.ino
@@ -5,13 +5,15 @@
 
 #define LED_TYPE WS2811
 #define COLOR_ORDER RGB
+#define LED 2
 
 CRGB leds[NUM_LEDS];
 uint8_t B_1 = 0;
-unsigned long Time = 3500;  
+unsigned long Time = 1500;  
 
 void setup() {
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS); 
+  pinMode(LED,OUTPUT);
 }
 
 void loop() {
@@ -19,11 +21,11 @@ void loop() {
   if ( millis() < Time){
 
   B_1 = beatsin8(30);
-
+  
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB(B_1,0,0);
   }
-
+  
   Serial.println(B_1);
   FastLED.show();
 
@@ -36,14 +38,16 @@ void loop() {
     
     for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB(150,0,0);
-    delay(80);
+    delay(10);
     FastLED.show();
     }
 
+    digitalWrite(LED,HIGH);
     delay(500);
     delay(100);
+    digitalWrite(LED,LOW);
   
-    Time = millis() + 3500;
+    Time = millis() + 1500;
     
   }
 
